42ecb2e156a567730152729377e02cc494465001
qt3r <angeloyap0329@gmail.com>
2026-02-10 22:18:37 -0800

    first commit

---
+++ src/main/java/com/assignment2/ratestaff/RatestaffApplication.java
@@ -0,0 +1,13 @@
+package com.assignment2.ratestaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatestaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatestaffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment2/ratestaff/RatestaffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment2.ratestaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatestaffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
